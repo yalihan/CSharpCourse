5a337c99ae0f27dc03621905d244ce96fe9ed5e3
coderyilmaz <coderyilmaz@gmail.com>
2020-07-19 19:35:28 +0300

    Updated

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Logger logger = new Logger();
+            logger.Log();
+            Console.WriteLine("Customer Added");
+        }
+    }
+    class Logger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+}
