baa69e8e91396179ed425be73f706a6fedd5eb20
coderyilmaz <coderyilmaz@gmail.com>
2020-07-20 12:03:23 +0300

    Added!

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Customer();
+            person.LastName = "Erisime acik";
+        }
+    }
+    public class Person
+    {
+        int Id;
+        protected string FirstName;
+        public string LastName;
+    }
+    class Customer : Person
+    {
+        public void Save()
+        {
+            FirstName = "Alihan";
+            LastName = "Yılmaz";
+        }
+    }
+    internal class Student : Person
+    {
+        public void Save()
+        {
+            LastName = "Yılmaz";
+        }
+    }
+}
---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+
+namespace AccessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.LastName = "Baska Proje Erisime Acik!";
+
+        }
+    }
+}
