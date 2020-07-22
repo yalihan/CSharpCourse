dbd9b2f2f40e7c4357da192370ece34f2f5df765
coderyilmaz <coderyilmaz@gmail.com>
2020-07-22 14:18:27 +0300

    Updated!

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -16,6 +16,8 @@ namespace Collections
             //WorkingWithArrayList();
             //WorkingWithGenericCollections();
             //WorkingWithGenericCollectionsAdvance();
+            //AddingElementToList();
+            SomeMethodsOfList();
             Console.ReadLine();
         }
         class Customer
@@ -24,8 +26,96 @@ namespace Collections
             public string FirstName { get; set; }
             public string toString()
             {
-                return "Id : "+Id +"\tFirst Name : "+FirstName;
+                return "Id : " + Id + "\tFirst Name : " + FirstName;
             }
+        }
+        private static void SomeMethodsOfList()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "Alihan" },
+                new Customer { Id = 2, FirstName = "Merve" }
+            };
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+
+            var customer3 = new Customer
+            {
+                Id = 3,
+                FirstName = "Omer"
+            };
+            Console.WriteLine("\n*****Customer3 ekledik******\n");
+            customers.Add(customer3);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+
+            Console.WriteLine("customer3 var mi ? :"+customers.Contains(customer3));
+            Console.WriteLine("new customer yapip ayni degerleri girersek var mi ? "+customers.Contains(new Customer { Id = 1, FirstName = "Alihan" }));
+
+            var index = customers.IndexOf(customer3);
+            Console.WriteLine("\nIndex of customer3 : "+index+"\n");
+
+            Console.WriteLine("\n***** customer3 elemanini bir daha ekledik *****\n");
+
+            customers.Add(customer3);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+
+            var lastIndex = customers.LastIndexOf(customer3);
+            Console.WriteLine("Last Index of customer3 : " + lastIndex);
+
+            Console.WriteLine("\n***** customer3 ogesini 1. indexe ekledik *****\n");
+            customers.Insert(1, customer3);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+
+            Console.WriteLine("\n***** customer3 lerden birini sildik *****\n");
+            customers.Remove(customer3);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+            Console.WriteLine("\n***** customer listesinde, ismi Omer olanlarin hepsini sildik *****\n");
+            customers.RemoveAll(c=>c.FirstName=="Omer");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+        }
+        private static void AddingElementToList()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "Alihan" },
+                new Customer { Id = 2, FirstName = "Merve" }
+            };
+
+            var customer3 = new Customer
+            {
+                Id=3,FirstName="Ömer"
+            };
+
+            customers.Add(customer3);
+            
+            customers.AddRange(new Customer[2]
+            { 
+                new Customer{Id=4,FirstName="Hamza" },
+                new Customer{Id=5,FirstName="Afra"}
+            });
+            
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.toString());
+            }
+            Console.WriteLine("Element Number of Customers List : {0}", customers.Count);
         }
         private static void WorkingWithGenericCollectionsAdvance()
         {
@@ -34,10 +124,10 @@ namespace Collections
             //customers.Add(new Customer { Id = 1, FirstName = "Merve" });
 
             List<Customer> customers = new List<Customer>
-            {
-                new Customer { Id = 0, FirstName = "Alihan" },
-                new Customer { Id = 1, FirstName = "Merve" }
-            };
+                {
+                    new Customer { Id = 0, FirstName = "Alihan" },
+                    new Customer { Id = 1, FirstName = "Merve" }
+                };
             foreach (var customer in customers)
             {
                 Console.WriteLine(customer.toString());
@@ -64,10 +154,9 @@ namespace Collections
             cities.Add('a');
             foreach (var city in cities)
             {
-                Console.WriteLine(city);
+                Console.WriteLine(city + " --> " + city.GetType());
             }
         }
-
         private static void WhyNeedCollections()
         {
             string[] cities = { "İstanbul", "Giresun" };
