1ca47f6eef33a35039d98dfd70603e3904bd0bc5
coderyilmaz <coderyilmaz@gmail.com>
2020-08-13 15:42:57 +0300

    Added!

---
+++ Attributes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, LastName = "Yilmaz", Age = 24 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
+    class Customer
+    {
+        [RequiredProperty]
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!",customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property)]
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+    }
+}
