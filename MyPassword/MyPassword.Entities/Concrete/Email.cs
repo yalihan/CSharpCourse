b3ceb6f231c889377a45ecba9f84ba675ee8fc03
coderyilmaz <coderyilmaz@gmail.com>
2020-09-10 11:57:10 +0300

    Entities Added!

---
+++ MyPassword/MyPassword.Entities/Concrete/Email.cs
@@ -0,0 +1,16 @@
+﻿using MyPassword.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPassword.Entities.Concrete
+{
+    public class Email : IEntity
+    {
+        public int EmailId { get; set; }
+        public int UserId { get; set; }
+        public string EmailName { get; set; }
+    }
+}
---
+++ MyPassword/MyPassword.Entities/Concrete/User.cs
@@ -0,0 +1,16 @@
+﻿using MyPassword.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPassword.Entities
+{
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
